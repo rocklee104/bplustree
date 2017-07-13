2707a9463a6a46e5be8d13dc2d5c8176884f7dd7
Rock Lee <rockdotlee@gmail.com>
2017-07-13 20:21:13 +0800

    add debug command

--- demo_build.sh
+++ demo_build.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-rm -rf build
-mkdir build
-cd build
-cmake ..
-make
-./bin/bplustree_demo
+GDB=cgdb
+BUILD_DIR=build
+
+function build_demo()
+{
+	rm -rf $BUILD_DIR
+	mkdir $BUILD_DIR
+	cd $BUILD_DIR
+	cmake ..
+	make
+	cd ..
+}
+
+build_demo
+if [ b_$1 == 'b_run' ]; then
+	$BUILD_DIR/bin/bplustree_demo
+elif [ b_$1 == 'b_debug' ]; then
+	$GDB $BUILD_DIR/bin/bplustree_demo
+fi
